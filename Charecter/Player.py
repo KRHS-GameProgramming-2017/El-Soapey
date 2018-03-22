8deb7a1483e2306501bad76e04e7d535ad31d759
xXMatternixXx <Zeb.santos@kearsarge.org>
2018-03-22 12:45:09 -0400

    Started Main.py. Started Player.py. Boss.py error fixed!

--- Charecter/Player.py
+++ Charecter/Player.py
@@ -1,1 +1,5 @@
+import pygame, math
 
+
+
+group Player(Player):
---
+++ Main.py
@@ -0,0 +1,16 @@
+import from player *
+import from npc *
+import from boss *
+import from score *
+import from health *
+import from background *
+pygame.init()
+
+clock = pygame.time.Clock()
+
+size = [width, height] = 800, 600
+screen = pygame.display.set_mode(size)
+
+bgColor = [r, g, b] = [138, 138, 138]
+
+
