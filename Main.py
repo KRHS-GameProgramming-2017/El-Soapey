463c1c0985c23c3c413d261978e87a23ef1e9afc
xXMatternixXx <Zeb.santos@kearsarge.org>
2018-03-23 13:22:32 -0400

    Edited and awaiting background updates.

--- Main.py
+++ Main.py
@@ -14,3 +14,11 @@ screen = pygame.display.set_mode(size)
 bgColor = [r, g, b] = [138, 138, 138]
 
 
+players = pygame.sprite.Group()
+blocks = pygame.sprite.Group()
+all = pygame.sprite.OrderedUpdates()
+
+Player.containers = (all, balls)
+Block.containers = (all, blocks)
+
+players = Player([random.randint(0,width/4), random.randint(0, height/5)])
